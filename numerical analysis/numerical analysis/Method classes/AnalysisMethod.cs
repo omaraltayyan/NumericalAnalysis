b8939da6c12f8929807140473f2051ca81793821
omar altayyan <omaraltayyan@gmail.com>
2015-05-02 11:03:16 +0300

    minor changes to code

--- numerical analysis/numerical analysis/Method classes/AnalysisMethod.cs
+++ numerical analysis/numerical analysis/Method classes/AnalysisMethod.cs
@@ -7,8 +7,12 @@ namespace numerical_analysis.Method_classes
 {
     abstract class AnalysisMethod
     {
-        public static int samplesXIndex = FormNumericalAnalysis.samplesXIndex;
-        public static int samplesYIndex = FormNumericalAnalysis.samplesYIndex;
+
+        // a convinient rename to shorten the terms for use in 
+        // subclasses (so that you don't have to write FormNumericalAnalysis.samplesXIndex every time
+        // u wanna reference these constants
+        public const int samplesXIndex = FormNumericalAnalysis.samplesXIndex;
+        public const int samplesYIndex = FormNumericalAnalysis.samplesYIndex;
 
         public bool isSolvable { get;protected set; }
 
